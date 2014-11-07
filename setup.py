e2cff1b92e4624550e12b527e6c35950474c8845
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-11-07 15:35:31 +0100

    Preparing release 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='paperclip',
-    version='0.1.2.dev0',
+    version='0.1.2',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/paperclip',
