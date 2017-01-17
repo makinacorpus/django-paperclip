39949ec5674a171b7be1a2093d2e158ca3a6ce4d
Gaël UTARD <gael.utard@makina-corpus.com>
2017-01-17 16:24:42 +0100

    Add test project and app

--- test_project/settings.py
+++ test_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'paperclip',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +101,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PAPERCLIP_ENABLE_VIDEO = True
