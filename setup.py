9a1706fae64cd17183308af258192d8d1006a1dc
J-E Castagnede <j.e.castagnede@gmail.com>
2017-03-22 16:59:28 +0100

    add external url field

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
                      open(os.path.join(here, 'CHANGES')).read(),
     license='LPGL, see LICENSE file.',
     install_requires=[
-        'Django',
+        'Django==1.8.17',
         'easy-thumbnails',
         'django-embed-video',
     ],
