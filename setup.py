f839b88427c65b3c31fa59066924ea46f55bc0af
J-E Castagnede <j.e.castagnede@gmail.com>
2017-03-23 15:38:27 +0100

    set version to 2.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='paperclip',
-    version='2.0.1',
+    version='2.1.0',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/paperclip',
