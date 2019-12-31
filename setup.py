3deb34d092d07c4d9b6b1d6381e82de8bbadf95f
Gaël UTARD <gael.utard@makina-corpus.com>
2019-12-31 16:16:05 +0100

    Preparing release 2.2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='paperclip',
-    version='2.2.3.dev0',
+    version='2.2.3',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/paperclip',
