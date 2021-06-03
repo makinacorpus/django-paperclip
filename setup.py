488cb72ed19287f6e5589c9e0c13a1a2284ade7a
J-E Castagnede <j.e.castagnede@gmail.com>
2021-06-03 12:24:12 +0200

    prepare release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests_require = [
 
 setup(
     name='paperclip',
-    version='2.3.0',
+    version='2.3.1',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-paperclip',
