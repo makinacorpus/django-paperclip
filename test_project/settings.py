495d756785027ea71959bfa2958397f8bf02affa
J-E Castagnede <j.e.castagnede@gmail.com>
2021-06-03 12:08:43 +0200

    improve tests

--- test_project/settings.py
+++ test_project/settings.py
@@ -107,3 +107,6 @@ STATIC_URL = '/static/'
 PAPERCLIP_ENABLE_VIDEO = True
 PAPERCLIP_FILETYPE_MODEL = 'test_app.FileType'
 PAPERCLIP_ATTACHMENT_MODEL = 'test_app.Attachment'
+
+# fix for project created django < 3.2
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
