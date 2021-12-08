1379cec4dcc861cf93fdd85fc72a81fc049a8fb1
J-E Castagnede <j.e.castagnede@gmail.com>
2021-12-08 11:00:48 +0100

    fix deprecation in django 4.0

--- test_project/test_app/urls.py
+++ test_project/test_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import TestView
 
 urlpatterns = [
-    path(r'^test_object/<int:pk>/', TestView.as_view()),
+    path('test_object/<int:pk>/', TestView.as_view()),
 ]
