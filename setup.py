521cfc9795f94ce61bf194c0a2ac1702dc011a84
Célia Prat <celia.prat@makina-corpus.com>
2023-02-15 10:50:49 +0100

    :bookmark: Prepare release 2.7.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests_require = [
 
 setup(
     name='paperclip',
-    version='2.6.1+dev',
+    version='2.7.0',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-paperclip',
