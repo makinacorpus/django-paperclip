1381f5308bce1c0431006a462acca896be8920b0
Célia Prat <celia.prat@makina-corpus.com>
2023-02-15 11:13:19 +0100

    :bookmark: Back to development

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests_require = [
 
 setup(
     name='paperclip',
-    version='2.7.0',
+    version='2.7.0+dev',
     author='Makina Corpus',
     author_email='geobi@makina-corpus.com',
     url='https://github.com/makinacorpus/django-paperclip',
